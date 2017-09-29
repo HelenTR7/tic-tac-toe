f46a20af85d3c667cce44cde131ed03077220026
Helen Trapeznikova <31996958+HelenTR7@users.noreply.github.com>
2017-09-29 21:23:33 +0300

    Update tic-tac-toe.js

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,24 +1,25 @@
 class TicTacToe {
     constructor() {
-    	this.field = [
+
+    	this.rez = 'x';
+    	this.kol = 0;
+    	this.pole = [
     	   [null, null, null],
     	   [null, null, null],
     	   [null, null, null],
     	];
-    	this.mark = 'x';
-    	this.filledCell = 0;
     }
 
     getCurrentPlayerSymbol() {
-    	return this.mark;
+    	return this.rez;
     }
 
     nextTurn(rowIndex, columnIndex) {
-    	if (this.field[rowIndex][columnIndex] == null) {
-    		this.filledCell++;
-    		this.field[rowIndex][columnIndex] = this.mark;
-    		if (this.mark == 'x') this.mark = 'o';
-    		else this.mark = 'x';
+    	if (this.pole[rowIndex][columnIndex] == null) {
+    		this.kol++;
+    		this.pole[rowIndex][columnIndex] = this.rez;
+    		if (this.rez == 'x') this.rez = 'o';
+    		else this.rez = 'x';
     	}
     }
 
@@ -28,46 +29,59 @@ class TicTacToe {
     }
 
     getWinner() {
-    	if
-    		((this.field[0][0] == 'x' && this.field[0][1] == 'x' && this.field[0][2] == 'x') ||
-    		(this.field[1][0] == 'x' && this.field[1][1] == 'x' && this.field[1][2] == 'x') ||
-    		(this.field[2][0] == 'x' && this.field[2][1] == 'x' && this.field[2][2] == 'x') ||
+    	if(
+    	    (this.pole[0][0] == 'x' && this.pole[0][1] == 'x' && this.pole[0][2] == 'x') ||
+    		(this.pole[1][0] == 'x' && this.pole[1][1] == 'x' && this.pole[1][2] == 'x') ||
+    		(this.pole[2][0] == 'x' && this.pole[2][1] == 'x' && this.pole[2][2] == 'x') ||
 
-    		(this.field[0][0] == 'x' && this.field[1][0] == 'x' && this.field[2][0] == 'x') ||
-    		(this.field[0][1] == 'x' && this.field[1][1] == 'x' && this.field[2][1] == 'x') ||
-    		(this.field[0][2] == 'x' && this.field[1][2] == 'x' && this.field[2][2] == 'x') ||
+    		(this.pole[0][0] == 'x' && this.pole[1][0] == 'x' && this.pole[2][0] == 'x') ||
+    		(this.pole[0][1] == 'x' && this.pole[1][1] == 'x' && this.pole[2][1] == 'x') ||
+    		(this.pole[0][2] == 'x' && this.pole[1][2] == 'x' && this.pole[2][2] == 'x') ||
 
-    		(this.field[0][0] == 'x' && this.field[1][1] == 'x' && this.field[2][2] == 'x') ||
-    		(this.field[2][0] == 'x' && this.field[1][1] == 'x' && this.field[0][2] == 'x'))
-    		return 'x';
-    	else if 
-    		((this.field[0][0] == 'o' && this.field[0][1] == 'o' && this.field[0][2] == 'o') ||
-    		(this.field[1][0] == 'o' && this.field[1][1] == 'o' && this.field[1][2] == 'o') ||
-    		(this.field[2][0] == 'o' && this.field[2][1] == 'o' && this.field[2][2] == 'o') ||
+    		(this.pole[0][0] == 'x' && this.pole[1][1] == 'x' && this.pole[2][2] == 'x') ||
+    		(this.pole[2][0] == 'x' && this.pole[1][1] == 'x' && this.pole[0][2] == 'x')
+    		)
+    		{return 'x';} 
+    	else 
+    	if (
+    		(this.pole[0][0] == 'o' && this.pole[0][1] == 'o' && this.pole[0][2] == 'o') ||
+    		(this.pole[1][0] == 'o' && this.pole[1][1] == 'o' && this.pole[1][2] == 'o') ||
+    		(this.pole[2][0] == 'o' && this.pole[2][1] == 'o' && this.pole[2][2] == 'o') ||
 
-    		(this.field[0][0] == 'o' && this.field[1][0] == 'o' && this.field[2][0] == 'o') ||
-    		(this.field[0][1] == 'o' && this.field[1][1] == 'o' && this.field[2][1] == 'o') ||
-    		(this.field[0][2] == 'o' && this.field[1][2] == 'o' && this.field[2][2] == 'o') ||
+    		(this.pole[0][0] == 'o' && this.pole[1][0] == 'o' && this.pole[2][0] == 'o') ||
+    		(this.pole[0][1] == 'o' && this.pole[1][1] == 'o' && this.pole[2][1] == 'o') ||
+    		(this.pole[0][2] == 'o' && this.pole[1][2] == 'o' && this.pole[2][2] == 'o') ||
 
-    		(this.field[0][0] == 'o' && this.field[1][1] == 'o' && this.field[2][2] == 'o') ||
-    		(this.field[2][0] == 'o' && this.field[1][1] == 'o' && this.field[0][2] == 'o'))
-    		return 'o';
+    		(this.pole[0][0] == 'o' && this.pole[1][1] == 'o' && this.pole[2][2] == 'o') ||
+    		(this.pole[2][0] == 'o' && this.pole[1][1] == 'o' && this.pole[0][2] == 'o')
+    		){
+    		return 'o';}
     	else return null;
     }
 
     noMoreTurns() {
-    	if (this.filledCell == 9) return true;
-    	else return false;
+    	if (this.kol == 9){
+    	 return true;
+    	}
+    	else { 
+    		return false;
+    	}
     }
 
     isDraw() {
-    	if (this.noMoreTurns() && !(this.getWinner())) return true;
-    	else return false;
+    	if (this.noMoreTurns() && !(this.getWinner())) 
+    	{
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
     }
 
     getFieldValue(rowIndex, colIndex) {
-    	return this.field[rowIndex][colIndex];
+    	return this.pole[rowIndex][colIndex];
     }
 }
 
 module.exports = TicTacToe;
+
